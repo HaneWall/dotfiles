bd4a9de715abe31b561b1378a1491c64d23a284c
HaneWall <48854946+HaneWall@users.noreply.github.com>
2022-11-01 07:55:53 +0100

    Init
    Full Gilles Castel nvim configuration

---
+++ nvim/lua/hannes/plugins/vimtex.lua
@@ -0,0 +1,7 @@
+-- might be necessary to enable snippets
+-- vim.g.vimtex_imaps_enabled = 0
+
+-- pdf viewer options
+vim.g.vimtex_view_method = "zathura"
+vim.g.tex_flavor="latex"
+vim.g.vimtex_quickfix_mode=0
